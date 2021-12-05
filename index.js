555eacf78a897142f40bd70678f5dcb1c8a956c6
Andrew <andrewjkenny121@gmail.com>
2021-12-05 15:18:18 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+const write = ["Ada", "Brendan", "Ali"];
+const event1 = "birthday";
+function writeCards(write, event1) {
+
+const returnArray = [];
+
+  for (let i = 0; i < write.length; i++) {
+    returnArray[i] = (`Thank you, ${write[i]}, for the wonderful ${event1} gift!`);
+    
+  }
+
+  return returnArray;
+}
+
+writeCards(write, event1);
+
+
+
+
+  for (let countdown = 10; countdown >= 0; countdown--) {
+    console.log(countdown);
+  }
+  
